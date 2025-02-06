9e1059d5d036f8ed854c474f940f7aa2fa4b17db
muthaiyanmani <muthaiyan.mb@zohocorp.com>
2025-02-06 09:40:27 +0530

    fix: auth iframe issue fix

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -5,7 +5,6 @@ import { cn } from "@/lib/utils";
 import Link from "next/link";
 import Script from "next/script";
 import { useEffect } from "react";
-// import "../../../public/assets/js/catalyst-sdk.js";
 
 // Extend the Window interface to include the catalyst property
 declare global {
@@ -22,21 +21,26 @@ export default function SignInPage() {
     const authDomain = process.env.NEXT_PUBLIC_AUTH_DOMAIN;
 
     useEffect(() => {
-        (window.catalyst as any).initApp({
-            project_Id: projectId,
-            zaid,
-            auth_domain: authDomain,
-            is_appsail: "true",
-            api_domain: ""
-        }, {
-            org_id: orgId
-        });
-
-        (window.catalyst as any).auth.signIn("signin-frame", {signin_providers_only : true});
+        loadAuthIframe();
     }, [projectId, zaid, orgId, authDomain]);
 
+    const loadAuthIframe = () => {
+        if (window.catalyst) {
+            (window.catalyst as any).initApp({
+                project_Id: projectId,
+                zaid,
+                auth_domain: authDomain,
+                is_appsail: "true",
+                api_domain: ""
+            }, {
+                org_id: orgId
+            });
+            (window.catalyst as any).auth.signIn("signin-frame", { signin_providers_only: true });
+        }
+    }
+
     return <div className={cn("flex flex-col gap-6")}>
-        <Script src="/assets/js/catalyst-sdk.js" strategy="beforeInteractive" />
+        <Script src="/assets/js/catalyst-sdk.js" strategy="afterInteractive" onLoad={loadAuthIframe} />
         <Card className="md:w-96 w-full">
             <CardHeader>
                 <CardTitle className="text-2xl">Sign In</CardTitle>
@@ -47,11 +51,11 @@ export default function SignInPage() {
             <CardContent>
                 <div className="h-80" id="signin-frame"></div>
                 <div className="mt-4 text-center text-sm">
-                            Create an account?{" "}
-                            <Link href={"/auth/signup"} className="underline underline-offset-4 cursor-pointer ml-1">
-                                Sign Up
-                            </Link>
-                        </div>
+                    Create an account?{" "}
+                    <Link href={"/auth/signup"} className="underline underline-offset-4 cursor-pointer ml-1">
+                        Sign Up
+                    </Link>
+                </div>
             </CardContent>
         </Card>
 
--- app/dashboard/files/page.tsx
+++ app/dashboard/files/page.tsx
@@ -1,5 +1,13 @@
+import { headers } from "next/headers";
+import catalyst from "zcatalyst-sdk-node";
 
 export default async function FilesPage() {
-   
+    const nextHeaders = await headers();
+    const reqHeaders = Object.fromEntries(nextHeaders.entries());
+    const fileMetaTableId = process.env.FILE_META_TABLE_ID!;
+
+    const app = catalyst.initialize({ headers: reqHeaders });
+    const files = await app.datastore().table(fileMetaTableId).getPagedRows();
+    console.log(files);
     return <h1>Files</h1>
 }
