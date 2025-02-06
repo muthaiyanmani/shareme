7960028a09d5437e71692496bac0571688d090d3
muthaiyanmani <muthaiyan.mb@zohocorp.com>
2025-02-06 06:08:24 +0530

    fix: unused import compilation issue

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,3 @@
-import { headers } from "next/headers";
-import catalyst from "zcatalyst-sdk-node";
-import { redirect } from 'next/navigation'
-import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
-import { AppSidebar } from "@/components/app-sidebar";
-import { Separator } from "@/components/ui/separator";
-import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
 
 export default async function DashboardHome() {
   
