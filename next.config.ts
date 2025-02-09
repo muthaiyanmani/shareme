a95cbb549a908cc0c0c59bc4789fdc30354376a5
muthaiyanmani <muthaiyan.mb@zohocorp.com>
2025-02-10 04:30:12 +0530

    fix: added multiple appurl support

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,13 @@
 import type { NextConfig } from "next";
 
 const { APP_URL } = process.env;
+const appUrls = APP_URL ? APP_URL?.split(',').map((url) => url.trim()) : [];
+
 const nextConfig: NextConfig = {
     output:"standalone",
     experimental:{
         serverActions:{
-            allowedOrigins:[APP_URL!]
+            allowedOrigins:[...appUrls]
         }
     }
 };
