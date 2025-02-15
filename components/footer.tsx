aaaa97e74d10aeef179ac85d46b59a2ac6c8f812
muthaiyanmani <muthaiyan.mb@zohocorp.com>
2025-02-15 20:23:22 +0530

    chore: added nanoid, added file upload size limit

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,6 +2,6 @@
 
 export default function Footer() {
     return <footer className="justify-center p-2 gap-1 flex text-sm text-white mt-4">
-        <p>built with love by</p> <a target="_blank" className="font-bold" href={`https://muthaiyan.pro?ref=${window.location.href}`}>muthaiyanmani</a>
+        <p>built with love by</p> <a target="_blank" className="font-bold" href={`https://muthaiyan.pro?ref=dropit`}>muthaiyanmani</a>
     </footer>
 }
